bf9f579552025eac43f42302b5b6044bfee57df9
odaniellevi <daniellevi.unicap@gmail.com>
2025-06-12 00:36:42 -0300

    criação das telas de login de contratante e trabalhador

--- projeto/src/app/testeD/page.js
+++ projeto/src/app/testeD/page.js
@@ -4,11 +4,13 @@ import EmployerSign from "../components/employerSign";
 import VacancyEmployer from "../components/vacancyEmployer";
 import VacancyAdd from "../components/vacancyAdd";
 import EditVacancy from "../components/editVacancy";
+import EmployerLogin from "../components/employerLogin";
+import EmployeeLogin from "../components/employeeLogin";
 
 export default function Home() {
   return (
     <>
-      <EmployerSign />
+      <EmployeeLogin />
     </>
   );
 }
