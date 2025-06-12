28811c2a74bab16e21a9213fa6b041e8e591c940
odaniellevi <daniellevi.unicap@gmail.com>
2025-06-11 22:58:00 -0300

    Update page.js

--- projeto/src/app/testeD/page.js
+++ projeto/src/app/testeD/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import VacancyPage from "../components/vacancyPage";
 import EmployerSign from "../components/employerSign";
+import VacancyEmployer from "../components/vacancyEmployer";
+import VacancyAdd from "../components/vacancyAdd";
+import EditVacancy from "../components/editVacancy";
 
 export default function Home() {
   return (
     <>
-      <EmployerSign />
+      <EditVacancy />
     </>
   );
 }
